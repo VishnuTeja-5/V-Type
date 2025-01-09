451a716c26e3264b65d3b6df93c1e9efb9943ce4
Vishnu <vishnutejakommera5@gmail.com>
2025-01-09 20:14:11 +0530

    fix: path for static files

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
-import App from './App.jsx'
+import App from "./App.jsx"
 import Layout from './Layout.jsx'
 import './index.css'
-import {CodeEditorWindow, CodingPage} from './Components';
+import { CodingPage} from './Components/index.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} >
       <Route index element={<App />} />
       <Route path='code' element = {<CodingPage />} />
-      <Route path='*' element = {<div>Page Not Found</div>} />
+      {/* <Route path='*' element = {<div className='text-white'>Page Not Found</div>} /> */}
     </Route>
-  )  
+  ),
+  { basename: '/V-Type/' }
 );
 
 createRoot(document.getElementById('root')).render(
